761b7d095c3cc06e7e1ce4fed071237fc99809eb
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2016-12-30 05:47:02 +0000

    Remove unused functions.

--- scripts/model.js
+++ scripts/model.js
@@ -37,27 +37,5 @@ var model = {
     ],
     nodeLoads: [
         {recid:0, node:3, x:10.0, y:0.0, z:0.0, rx:0.0, ry:0.0, rz:0.0}
-    ],
-    nodeFromId: function(id){
-        return this.nodes.find(function(t){
-            return t.recid === id;
-        });
-    },
-    lineFromId: function(id){
-        return this.lines.find(function(t){
-            return t.recid === id;
-        });
-    },
-    lineLength: function(id){
-        var line = this.lineFromId(id);
-        var n1 = this.nodeFromId(line.n1);
-        var n2 = this.nodeFromId(line.n2);
-        return Math.sqrt((n1.x-n2.x)**2 + (n1.y-n2.y)**2 + (n1.z-n2.z)**2);
-    },
-    lineVector: function(id){
-        var line = this.lineFromId(id);
-        var n1 = this.nodeFromId(line.n1);
-        var n2 = this.nodeFromId(line.n2);
-        return {x:n2.x-n1.x, y:n2.y-n1.y, z:n2.z-n1.z};
-    }
+    ]
 };
