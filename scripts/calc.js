ae6aa313ad109a4562014105c3bba2732f173bdb
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-01-03 03:07:57 +0000

    Fix the size of result grid, and make sure to display result correctly when calculate button clicked second and more times.

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,6 +1,38 @@
 /*eslint-env jquery*/
 /*globals w2popup model*/
 
+function parseResponse(response){
+    var r = [];
+    var d = response.result.displacements;
+    for (p in d){
+        r.push({recid:p, x:d[p].x, y:d[p].y, z:d[p].z});
+    }
+    return r;
+}
+
+$().w2grid(
+    $.extend(gridOptions,{
+        name: 'displacements',
+        header: 'Displacements',
+        show: {
+            toolbar: true,
+            footer: true,
+            toolbarReload: false
+        },
+        columns: [
+            { field: 'recid', caption: 'ID', size: '10%', sortable: true, resizable: true },
+            { field: 'x', caption: 'dx', size: '30%', sortable: true, resizable: true },
+            { field: 'y', caption: 'dy', size: '30%', sortable: true, resizable: true },
+            { field: 'z', caption: 'dz', size: '30%', sortable: true, resizable: true }
+        ],
+        searches: [
+            { field: 'x', caption: 'X', type: 'float' },
+            { field: 'y', caption: 'Y', type: 'float' },
+            { field: 'z', caption: 'Z', type: 'float' }
+        ]
+    })
+);
+
 $('#calculate').click(function(){
     var btn = $(this).attr("disabled", true);
     $.ajax({
@@ -17,43 +49,17 @@ $('#calculate').click(function(){
         }),
         dataType: "json",
         success: function(response){
+            w2ui['displacements'].records = parseResponse(response);
             w2popup.open({
                 title:'Result',
-                body:'<div id="popup_res" style="height:100%;width:100%"></div>'
+                body:'<div id="popup_res" style="height:100%;width:100%"></div>',
+                onOpen: function(e){
+                    e.onComplete=function(){
+                        $('#popup_res').w2render('displacements');
+                        w2ui['displacements'].refresh();
+                    }
+                }
             });
-            var r = [];
-            var d = response.result.displacements;
-            for (p in d){
-                r.push({recid:p, x:d[p].x, y:d[p].y, z:d[p].z});
-            }
-            if (w2ui['displacements']){
-                w2ui['displacements'].records = r;
-                w2ui['displacements'].refresh();
-            } else {
-            $('#popup_res').w2grid(
-                $.extend(gridOptions,{
-                    name: 'displacements',
-                    header: 'Displacements',
-                    show: {
-                        toolbar: true,
-                        footer: true,
-                        toolbarReload: false
-                    },
-                    columns: [
-                        { field: 'recid', caption: 'ID', size: '10%', sortable: true, resizable: true },
-                        { field: 'x', caption: 'dx', size: '30%', sortable: true, resizable: true },
-                        { field: 'y', caption: 'dy', size: '30%', sortable: true, resizable: true },
-                        { field: 'z', caption: 'dz', size: '30%', sortable: true, resizable: true }
-                    ],
-                    searches: [
-                        { field: 'x', caption: 'X', type: 'float' },
-                        { field: 'y', caption: 'Y', type: 'float' },
-                        { field: 'z', caption: 'Z', type: 'float' }
-                    ],
-                    records: r
-                })
-            ).refresh();
-            }
         },
         error: function(){
             w2popup.open({
