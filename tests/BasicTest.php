b27d6c441b50ba9d4208fa2500213ea059523c45
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-01-13 15:19:04 +0000

    Add test template using PHPUnit.

---
+++ tests/BasicTest.php
@@ -0,0 +1,8 @@
+<?php
+
+class SimpleTest extends PHPUnit_Framework_TestCase {
+    public function testBoolean() {
+        $this->assertEquals(True, True);
+        $this->assertEquals(False, False);
+    }
+}
