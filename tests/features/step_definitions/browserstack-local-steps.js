9fce7d5659b688eebcfbaff62d495d5451c35b50
池田大樹 <ikeda_hiroki@ikedadaikinoMacBook-Pro.local>
2017-02-09 23:25:54 +0900

    browserstack-local test runs correct.

---
+++ tests/features/step_definitions/browserstack-local-steps.js
@@ -0,0 +1,20 @@
+/*eslint-env node */
+
+var {defineSupportCode} = require('cucumber');
+var assert = require('cucumber-assert');
+var webdriver = require('selenium-webdriver');
+
+defineSupportCode(function({Then, When}) {
+	
+	When(/^I open site$/, function (next) {
+        this.driver.get('http://127.0.0.1/');
+        next();
+    });
+
+    Then(/^I should see title "([^"]*)"$/, function (titleMatch, next) {
+        this.driver.getTitle().then(function(title) {
+            assert.equal(title, titleMatch, next, 'Expected title to be ' + titleMatch);
+        });
+    });
+
+});
---
+++ tests/features/support/hooks.js
@@ -0,0 +1,64 @@
+var {defineSupportCode} = require('cucumber');
+
+var webdriver = require("selenium-webdriver");
+var browserstack = require("browserstack-local");
+
+var createBrowserStackSession = function(config, caps){
+  return new webdriver.Builder().
+    usingServer('http://'+config.server+'/wd/hub').
+    withCapabilities(caps).
+    build();
+}
+
+var config = {
+    server: 'hub-cloud.browserstack.com',
+    user: "hirokiikeda1",
+    key: "1JSwFjEmHAJzMRYtyp6q",
+    capabilities: [
+        {
+            browserName: 'chrome',
+            build: process.env.CIRCLE_BUILD_NUM,
+            project: process.env.CIRCLE_PROJECT_REPONAME + '-' + process.env.CIRCLE_BRANCH,
+            'browserstack.local': true
+        }
+    ]
+};
+
+var username = process.env.BS_USERNAME || config.user;
+var accessKey = process.env.BS_AUTHKEY || config.key;
+
+defineSupportCode(function({After, Before}) {
+  var bs_local = null;
+
+  Before(function (scenario, callback) {
+    var world = this;
+    var task_id = parseInt(process.env.TASK_ID || 0, 10);
+    var caps = config.capabilities[task_id];
+    caps['browserstack.user'] = username;
+    caps['browserstack.key'] = accessKey;
+
+    if(caps["browserstack.local"]){
+      // Code to start browserstack local before start of test and stop browserstack local after end of test
+      bs_local = new browserstack.Local();
+      bs_local.start({'key': accessKey }, function(error) {
+        if (error) return console.log(error.red);
+
+        world.driver = createBrowserStackSession(config, caps);
+        callback();
+      });
+    }
+    else {
+      world.driver = createBrowserStackSession(config, caps);
+      callback();
+    }
+  });
+
+  After(function(scenario, callback){
+    this.driver.quit().then(function(){
+      if(bs_local){
+        bs_local.stop(callback);
+      }
+      else callback();
+    });
+  });
+});
