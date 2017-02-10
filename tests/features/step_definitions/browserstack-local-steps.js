05b6b08dffe28f40257a5d1d493f5c0d471ece91
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2017-02-10 09:21:57 +0900

    Update browserstack-local-steps.js

--- tests/features/step_definitions/browserstack-local-steps.js
+++ tests/features/step_definitions/browserstack-local-steps.js
@@ -1,20 +1,20 @@
 /*eslint-env node */
 
-var defineSupportCode = require('cucumber').defineSupportCode;
-var assert = require('cucumber-assert');
-var webdriver = require('selenium-webdriver');
+var defineSupportCode = require("cucumber").defineSupportCode;
+var assert = require("cucumber-assert");
+var webdriver = require("selenium-webdriver");
 
 defineSupportCode(function(context) {
-    var Then = context.Then;
-    var When = context.When;
-    When(/^I open site$/, function (next) {
-        this.driver.get('http://127.0.0.1/');
+    var then = context.Then;
+    var when = context.When;
+    when(/^I open site$/, function (next) {
+        this.driver.get("http://127.0.0.1/");
         next();
     });
 
-    Then(/^I should see title "([^"]*)"$/, function (titleMatch, next) {
+    then(/^I should see title "([^"]*)"$/, function (titleMatch, next) {
         this.driver.getTitle().then(function(title) {
-            assert.equal(title, titleMatch, next, 'Expected title to be ' + titleMatch);
+            assert.equal(title, titleMatch, next, "Expected title to be " + titleMatch);
         });
     });
 
