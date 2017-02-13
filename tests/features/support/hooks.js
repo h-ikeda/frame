41ed34e79a4848a0fb652867586063e911d2ebe8
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2017-02-13 14:01:21 +0900

    Export configurations.

--- tests/features/support/hooks.js
+++ tests/features/support/hooks.js
@@ -2,129 +2,13 @@ var defineSupportCode = require("cucumber").defineSupportCode;
 
 var webdriver = require("selenium-webdriver");
 var browserstack = require("browserstack-local");
+var config = require("../../../config");
 
 var createBrowserStackSession = function(config, caps){
   return new webdriver.Builder().
     usingServer("http://"+config.server+"/wd/hub").
     withCapabilities(caps).
     build();
-};
-
-var config = {
-    server: "hub-cloud.browserstack.com",
-    capabilities: [
-        {
-            browserName: "Chrome",
-            browserVersion: "56.0",
-            os: "Windows",
-            os_vertion: "10",
-            resolution: "1024x768"
-        },
-        {
-            browserName: "firefox",
-            browserVersion: "52.0 beta",
-            os: "Windows",
-            os_vertion: "10",
-            resolution: "1024x768"
-        },
-        {
-            browserName: "IE",
-            browserVersion: "11.0",
-            os: "Windows",
-            os_vertion: "10",
-            resolution: "1024x768"
-        },
-        {
-            browserName: "Edge",
-            browserVersion: "14.0",
-            os: "Windows",
-            os_vertion: "10",
-            resolution: "1024x768"
-        },
-        {
-            browserName: "Safari",
-            browserVersion: "10.0",
-            os: "OS X",
-            os_vertion: "Sierra",
-            resolution: "1024x768"
-        },
-        {
-            browserName: "Firefox",
-            browserVersion: "52.0 beta",
-            os: "OS X",
-            os_vertion: "Sierra",
-            resolution: "1024x768"
-        },
-        {
-            browserName: "Chrome",
-            browserVersion: "56.0",
-            os: "OS X",
-            os_vertion: "Sierra",
-            resolution: "1024x768"
-        },
-        {
-            browserName: "Opera",
-            browserVersion: "12.15",
-            os: "OS X",
-            os_vertion: "Sierra",
-            resolution: "1024x768"
-        },
-        {
-            browserName: "iPhone",
-            platform: "MAC",
-            device: "iPhone 6S Plus"
-        },
-        {
-            browserName: "iPad",
-            platform: "MAC",
-            device: "iPad Mini 4"
-        },
-        {
-            browserName: "iPad",
-            platform: "MAC",
-            device: "iPad Pro"
-        },
-        {
-            browserName: "iPad",
-            platform: "MAC",
-            device: "iPad Air 2"
-        },
-        {
-            browserName: "android",
-            platform: "ANDROID",
-            device: "Samsung Galaxy S5"
-        },
-        {
-            browserName: "android",
-            platform: "ANDROID",
-            device: "HTC One M8"
-        },
-        {
-            browserName: "android",
-            platform: "ANDROID",
-            device: "Motorola Razr"
-        },
-        {
-            browserName: "android",
-            platform: "ANDROID",
-            device: "Sony Xperia Tipo"
-        },
-        {
-            browserName: "android",
-            platform: "ANDROID",
-            device: "Google Nexus 5"
-        },
-        {
-            browserName: "android",
-            platform: "ANDROID",
-            device: "Amazon Kindle Fire 2"
-        },
-        {
-            browserName: "android",
-            platform: "ANDROID",
-            device: "Samsung Galaxy Tab 4 10.1"
-        }
-    ]
 };
 
 var username = process.env.BS_USERNAME || config.user;
