a12f94252f24b0f4525dffeb7e9b6d45586b7925
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2017-02-13 14:03:02 +0900

    Create cucumber-runner.js

---
+++ tests/cucumber-runner.js
@@ -0,0 +1,13 @@
+var child_process = require("child_process");
+var config = require("./browserstack_config.js");
+
+process.argv[0] = "node";
+process.argv[1] = "cucumber-js";
+process.argv[2] = "tests/features";
+
+for(var i in config.capabilities){
+  var env = Object.create( process.env );
+  env.TASK_ID = i.toString();
+  var p = child_process.spawn('/usr/bin/env', process.argv, { env: env } );  
+  p.stdout.pipe(process.stdout);
+}
