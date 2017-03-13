806986011782d971bd63e7f9b54cdffd95245ed1
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2017-03-13 17:09:56 +0900

    マルチブラウザテストを設定しました。Frameworkは、webdriverIO + mocha.

--- tests/specs/display_test.js
+++ tests/specs/display_test.js
@@ -6,7 +6,7 @@ var assert = require("assert");
 describe("アプリの読み込みと表示", function() {
     it("タイトルは「Frame | 1stop-st.org」", function () {
         browser
-            .url("http://127.0.0.1:8080")
+            .url("http://localhost:8080/")
             .pause(5000);
         assert(browser.getTitle().match(/Frame | 1stop-st.org/i));
     });
