d8a9623719c6f56690ef1647e06338a05facc7a1
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-03-13 09:38:49 +0000

    断面と材料のデータテーブルを追加。

--- src/scripts/headers.js
+++ src/scripts/headers.js
@@ -45,21 +45,106 @@ module.exports = [{
     }, {
         field: "n1",
         caption: "Node1",
-        size: "30%",
+        size: "22.5%",
         editable: {
             type: "int"
         }
     }, {
         field: "n2",
         caption: "Node2",
-        size: "30%",
+        size: "22.5%",
         editable: {
             type: "int"
         }
     }, {
-        field: "EA",
-        caption: "EA",
-        size: "30%",
+        field: "section",
+        caption: "Section",
+        size: "22.5%",
+        editable: {
+            type: "int"
+        }
+    }, {
+        field: "material",
+        caption: "Material",
+        size: "22.5%",
+        editable: {
+            type: "int"
+        }
+    }]
+}, {
+    field: "materials",
+    caption: "Materials",
+    columns: [{
+        field: "recid",
+        caption: "ID",
+        size: "10%",
+        editable: {
+            type: "int"
+        }
+    }, {
+        field: "E",
+        caption: "E",
+        size: "45%",
+        editable: {
+            type: "float"
+        }
+    }, {
+        field: "G",
+        caption: "G",
+        size: "45%",
+        editable: {
+            type: "float"
+        }
+    }]
+}, {
+    field: "sections",
+    caption: "Sections",
+    columns: [{
+        field: "recid",
+        caption: "ID",
+        size: "10%",
+        editable: {
+            type: "int"
+        }
+    }, {
+        field: "shape",
+        caption: "Shape",
+        size: "10%",
+        editable: {
+            type: "text"
+        }
+    }, {
+        field: "H",
+        caption: "H",
+        size: "18%",
+        editable: {
+            type: "float"
+        }
+    }, {
+        field: "B",
+        caption: "B",
+        size: "18%",
+        editable: {
+            type: "float"
+        }
+    }, {
+        field: "tw",
+        caption: "tw",
+        size: "18%",
+        editable: {
+            type: "float"
+        }
+    }, {
+        field: "tf",
+        caption: "tf",
+        size: "18%",
+        editable: {
+            type: "float"
+        }
+    }, {
+        field: "r",
+        caption: "r",
+        size: "18%",
         editable: {
             type: "float"
         }
