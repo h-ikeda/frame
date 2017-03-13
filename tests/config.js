adb4fd3d0d66382d12ad08f5058d5297e4ee4f34
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-03-13 05:53:47 +0000

    マルチブラウザテストを設定しました。Frameworkは、webdriverIO + mocha.

--- tests/config.js
+++ tests/config.js
@@ -24,111 +24,113 @@ var commonCapabilities = {
 
 var capabilities = [
     {
-        os: 'Windows',
-        os_version: '10',
-        browser: 'IE',
-        browser_version: '11.0',
-        resolution: '1024x768'
+        os: "Windows",
+        os_version: "10",
+        browser: "IE",
+        browser_version: "11.0",
+        resolution: "1024x768"
     }, {
-        os: 'Windows',
-        os_version: '10',
-        browser: 'Edge',
-        browser_version: '14.0',
-        resolution: '1024x768'
+        os: "Windows",
+        os_version: "10",
+        browser: "Edge",
+        browser_version: "14.0",
+        resolution: "1024x768"
     }, {
-        os: 'Windows',
-        os_version: '10',
-        browser: 'Firefox',
-        browser_version: '53.0 beta',
-        resolution: '1024x768'
+        os: "Windows",
+        os_version: "10",
+        browser: "Firefox",
+        browser_version: "53.0 beta",
+        resolution: "1024x768"
     }, {
-        os: 'Windows',
-        os_version: '10',
-        browser: 'Chrome',
-        browser_version: '56.0',
-        resolution: '1024x768'
+        os: "Windows",
+        os_version: "10",
+        browser: "Chrome",
+        browser_version: "56.0",
+        resolution: "1024x768"
     }, {
-        os: 'OS X',
-        os_version: 'Sierra',
-        browser: 'Safari',
-        browser_version: '10.0',
-        resolution: '1024x768'
+        os: "OS X",
+        os_version: "Sierra",
+        browser: "Safari",
+        browser_version: "10.0",
+        resolution: "1024x768"
     }, {
-        os: 'OS X',
-        os_version: 'Sierra',
-        browser: 'Firefox',
-        browser_version: '53.0 beta',
-        resolution: '1024x768'
+        os: "OS X",
+        os_version: "Sierra",
+        browser: "Firefox",
+        browser_version: "53.0 beta",
+        resolution: "1024x768"
     }, {
-        os: 'OS X',
-        os_version: 'Sierra',
-        browser: 'Chrome',
-        browser_version: '56.0',
-        resolution: '1024x768'
+        os: "OS X",
+        os_version: "Sierra",
+        browser: "Chrome",
+        browser_version: "56.0",
+        resolution: "1024x768"
     }, {
-        os: 'OS X',
-        os_version: 'Sierra',
-        browser: 'Opera',
-        browser_version: '12.15',
-        resolution: '1024x768'
+        os: "OS X",
+        os_version: "Sierra",
+        browser: "Opera",
+        browser_version: "12.15",
+        resolution: "1024x768"
     }, {
-        browserName: 'iPhone',
-        platform: 'MAC',
-        device: 'iPhone 6S'
-    }/*, {
-        browserName: 'iPhone',
-        platform: 'MAC',
-        device: 'iPhone 6S Plus'
+        browserName: "iPhone",
+        platform: "MAC",
+        device: "iPhone 6S"
+    },/* {
+        browserName: "iPhone",
+        platform: "MAC",
+        device: "iPhone 6S Plus"
     }, {
-        browserName: 'iPad',
-        platform: 'MAC',
-        device: 'iPad Air 2'
+        browserName: "iPad",
+        platform: "MAC",
+        device: "iPad Air 2"
     }, {
-        browserName: 'iPad',
-        platform: 'MAC',
-        device: 'iPad Pro'
+        browserName: "iPad",
+        platform: "MAC",
+        device: "iPad Pro"
     }, {
-        browserName: 'iPad',
-        platform: 'MAC',
-        device: 'iPad Mini 4'
-    }*/, {
-        browserName: 'android',
-        platform: 'ANDROID',
-        device: 'Samsung Galaxy S5 Mini'
-    }/*, {
-        browserName: 'android',
-        platform: 'ANDROID',
-        device: 'Samsung Galaxy S5'
+        browserName: "iPad",
+        platform: "MAC",
+        device: "iPad Mini 4"
+    }, */{
+        browserName: "android",
+        platform: "ANDROID",
+        device: "Samsung Galaxy S5 Mini"
+    },/* {
+        browserName: "android",
+        platform: "ANDROID",
+        device: "Samsung Galaxy S5"
     }, {
-        browserName: 'android',
-        platform: 'ANDROID',
-        device: 'HTC One M8'
+        browserName: "android",
+        platform: "ANDROID",
+        device: "HTC One M8"
     }, {
-        browserName: 'android',
-        platform: 'ANDROID',
-        device: 'Motorola Razr Maxx HD'
+        browserName: "android",
+        platform: "ANDROID",
+        device: "Motorola Razr Maxx HD"
     }, {
-        browserName: 'android',
-        platform: 'ANDROID',
-        device: 'Sony Xperia Tipo'
+        browserName: "android",
+        platform: "ANDROID",
+        device: "Sony Xperia Tipo"
     }, {
-        browserName: 'android',
-        platform: 'ANDROID',
-        device: 'Google Nexus 5'
+        browserName: "android",
+        platform: "ANDROID",
+        device: "Google Nexus 5"
     }, {
-        browserName: 'android',
-        platform: 'ANDROID',
-        device: 'Amazon Kindle Fire HDX 7'
+        browserName: "android",
+        platform: "ANDROID",
+        device: "Amazon Kindle Fire HDX 7"
     }, {
-        browserName: 'android',
-        platform: 'ANDROID',
-        device: 'Samsung Galaxy Tab 4 10.1'
+        browserName: "android",
+        platform: "ANDROID",
+        device: "Samsung Galaxy Tab 4 10.1"
     }*/
 ];
 
 capabilities.forEach(function(cap) {
     for (var i in commonCapabilities) {
-        cap[i] = cap[i] || commonCapabilities[i];
+        if (commonCapabilities.hasOwnProperty(i)) {
+            cap[i] = cap[i] || commonCapabilities[i];
+        }
     }
 });
 
--- tests/specs/display_test.js
+++ tests/specs/display_test.js
@@ -1,11 +1,11 @@
-/*eslint-env mocha, node*/
+/*eslint-env node, mocha, webdriverio/wdio*/
 
 var assert = require("assert");
 
 describe("アプリの読み込みと表示", function() {
     it("タイトルは「Frame | 1stop-st.org」", function () {
         browser
-            .url('http://127.0.0.1:8080')
+            .url("http://127.0.0.1:8080")
             .pause(5000);
         assert(browser.getTitle().match(/Frame | 1stop-st.org/i));
     });
