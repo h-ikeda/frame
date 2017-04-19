e391d852ff5b640764ac5d08a5414fc8a7db792c
h-ikeda <ikeda_hiroki@icloud.com>
2017-04-19 14:14:44 +0900

    メニューリストを折り畳めるようにしました。

--- src/components/drawer/menulist.vue
+++ src/components/drawer/menulist.vue
@@ -1,10 +1,20 @@
 <template>
     <nav class="mdc-temporary-drawer__content mdc-list-group">
-        <h3 class="mdc-list-group__subheader">Input</h3>
-        <menulist-input></menulist-input>
+        <h3 class="mdc-list-group__subheader" @click="toggleInput">
+            Input
+            <i class="material-icons">
+                {{inputOpen ? "expand_less": "expand_more"}}
+            </i>
+        </h3>
+        <menulist-input v-show="inputOpen"></menulist-input>
         <hr class="mdc-list-divider">
-        <h3 class="mdc-list-group__subheader">Result</h3>
-        <menulist-result></menulist-result>
+        <h3 class="mdc-list-group__subheader" @click="toggleResult">
+            Result
+            <i class="material-icons">
+                {{resultOpen ? "expand_less": "expand_more"}}
+            </i>
+        </h3>
+        <menulist-result v-show="resultOpen"></menulist-result>
         <div class="menu-spacer"></div>
         <hr class="mdc-list-divider">
         <menulist-command></menulist-command>
@@ -16,6 +26,20 @@
     import result from "./menulist-result.vue";
     import command from "./menulist-command.vue";
     export default {
+        data() {
+            return {
+                inputOpen: false,
+                resultOpen: false
+            };
+        },
+        methods: {
+            toggleInput() {
+                this.inputOpen = !this.inputOpen;
+            },
+            toggleResult() {
+                this.resultOpen = !this.resultOpen;
+            }
+        },
         components: {
             "menulist-input": input,
             "menulist-result": result,
@@ -31,5 +55,11 @@
     }
     .menu-spacer {
         flex-grow: 1;
+    }
+    .mdc-list-group__subheader:hover {
+        cursor: pointer;
+    }
+    .material-icons {
+        vertical-align: middle;
     }
 </style>
