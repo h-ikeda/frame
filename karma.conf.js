534f83d68422da801099ae58a950c4839dfecffb
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2017-05-15 08:03:27 +0000

    BrowserStack使用時の設定エラーを解消し、各項目の説明を付加しました。

--- karma.conf.js
+++ karma.conf.js
@@ -1,42 +1,54 @@
 module.exports = function(config) {
+    //
+    // BrowserStackの設定をkarma-browsers.jsから読み込みます。
+    //
+    var customLaunchers = {};
+    var browsers = require("./karma-browsers").map(function(cap) {
+        // baseプロパティにBrowserStackを設定（karma-browserstack-launcher)。
+        cap.base = "BrowserStack";
+        // テストブラウザ名は設定内容から自動生成します。
+        var browser = "device" in cap ? cap.device:
+            cap.browser + " " +
+            cap.browser_version + " on " +
+            cap.os + " " +
+            cap.os_version + " resolution:" +
+            cap.resolution;
+        customLaunchers[browser] = cap;
+        return browser;
+    }).filter(function(_, index) {
+        // CircleCI上でパラレルテストが有効なとき、テストブラウザを分散します。
+        return process.env.CIRCLE_NODE_TOTAL < 2 || index % process.env.CIRCLE_NODE_TOTAL === parseInt(process.env.CIRCLE_NODE_INDEX);
+    });
+    //
+    // Karmaの設定。
+    //
     config.set({
+        // Framework: mocha
         frameworks: ["mocha"],
         reporters: ["mocha"],
+        // テストエントリファイル
         files: ["test/test_index.js"],
+        // バンドラを設定（webpack）
+        // 設定はwebpack.config.jsを読み込む（karma-webpack）
         preprocessors: {
             "test/test_index.js": ["webpack", "sourcemap"]
         },
-        beforeMiddleware: ["webpackBlocker"],
-        middleware: ["firebaseServer"],
         webpack: require("./webpack.config"),
+        // auto watch時にテストとバンドルのタイミングを自動調整してくれる。
+        beforeMiddleware: ["webpackBlocker"],
+        // firebaseのモックサーバー（karma-firebase-server）
+        middleware: ["firebaseServer"],
+        // ブラウザの同時接続数。BrowserStack使用時は1ブラウザずつに制限する。
         concurrency: process.env.BS_USERNAME && process.env.BS_AUTHKEY ? 1: Infinity,
-        browsers: process.env.BS_USERNAME && process.env.BS_AUTHKEY ? (function(keys) {
-            keys.sort();
-            return keys;
-        }(Object.keys(this.customLaunchers))).filter(function(value, index) {
-            return process.env.CIRCLE_NODE_TOTAL < 2 || index % process.env.CIRCLE_NODE_TOTAL === parseInt(process.env.CIRCLE_NODE_INDEX);
-        }): [],
+        // BrowserStackのユーザーIDとパスキーが設定されていればBrowserStack用の設定を使う。
+        browsers: process.env.BS_USERNAME && process.env.BS_AUTHKEY ? browsers: [],
+        // BrowserStack用の設定（karma-browserstack-launcher）
         browserStack: {
             username: process.env.BS_USERNAME,
             accessKey: process.env.BS_AUTHKEY,
-            project: process.env.CIRCLE_PROJECT_REPONAME + "_" + process.env.CIRCLE_BRANCH || "frame_local",
+            project: (process.env.CIRCLE_PROJECT_REPONAME || "frame") + "_" + (process.env.CIRCLE_BRANCH || require("child_process").execSync("git branch | grep \\* | cut -d \" \" -f2-")),
             build: process.env.CIRCLE_BUILD_NUM || Date.now()
         },
-        customLaunchers: (function(arr) {
-            var launchers = {};
-            arr.forEach(function(obj) {
-                var name = "device" in obj ? obj.device:
-                    obj.browser + " " +
-                    obj.browser_version + " on " +
-                    obj.os + " " +
-                    obj.os_version + " resolution:" +
-                    obj.resolution;
-                launchers[name] = obj;
-            });
-            return launchers;
-        }(require("./karma-browsers").map(function(obj) {
-            obj.base = "BrowserStack";
-            return obj;
-        })))
+        customLaunchers: customLaunchers
     });
 };
