2d5147e6000e77beb892076bfb668f8a60339056
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2017-05-23 04:19:41 +0000

    Complexity を減らしました。

--- karma.conf.js
+++ karma.conf.js
@@ -1,38 +1,56 @@
 "use strict";
-let bsCaps;
+
+const frameworks = ["mocha"];
+const reporters = ["coverage-istanbul", "progress"];
+const files = ["test/test_index.js"];
+const preprocessors = {
+    "test/test_index.js": ["webpack", "sourcemap"]
+};
+const webpack = require("./webpack.config");
+const beforeMiddleware = ["webpackBlocker"];
+let concurrency = Infinity;
+const browsers = [];
+const customLaunchers = {};
+const browserStack = {
+    project: "frame_" + require("child_process").execSync("git branch | grep \\* | cut -d \" \" -f2-")
+};
+
 if (process.env.BROWSER_STACK_USERNAME) {
-    bsCaps = require("browserstack-capabilities")(process.env.BROWSER_STACK_USERNAME, process.env.BROWSER_STACK_ACCESS_KEY);
+    reporters.push("BrowserStack");
+    concurrency = 1;
+    const bsCaps = require("browserstack-capabilities")(process.env.BROWSER_STACK_USERNAME, process.env.BROWSER_STACK_ACCESS_KEY);
+    const capabilities = bsCaps.create({
+        "browser": ["chrome", "firefox"],
+        "browser_version": "latest"
+    });
+    capabilities.forEach((capability) => {
+        const browser = Object.keys(capability).map((key) => capability[key]).filter((str) => str).join();
+        browsers.push(browser);
+        capability.base = "BrowserStack";
+        customLaunchers[browser] = capability;
+    });
+}
+
+if (process.env.CIRCLECI) {
+    browsers.sort();
+    const browserNum = Math.ceil(browsers.length / process.env.CIRCLE_NODE_TOTAL);
+    browsers.splice(0, browserNum * process.env.CIRCLE_NODE_INDEX);
+    browsers.splice(browserNum * (process.env.CIRCLE_NODE_INDEX + 1));
+    browserStack.project = process.env.CIRCLE_PROJECT_REPONAME + "_" + process.env.CIRCLE_BRANCH
 }
 
 module.exports = (config) => {
     let customLaunchers = {};
     config.set({
-        frameworks: ["mocha"],
-        reporters: ["coverage-istanbul", "progress", "BrowserStack"].slice(0, bsCaps ? 3: 2),
-        files: ["test/test_index.js"],
-        preprocessors: {
-            "test/test_index.js": ["webpack", "sourcemap"]
-        },
-        webpack: require("./webpack.config"),
-        beforeMiddleware: ["webpackBlocker"],
-        concurrency: bsCaps ? 1: Infinity,
-        browsers: bsCaps ? bsCaps.create({
-            browser: ["chrome", "firefox"],
-            "browser_version": "latest"
-        }).map((cap) => {
-            cap.base = "BrowserStack";
-            var browser = ("device" in cap && cap.device) ? cap.device:
-                (cap.browser + " " +
-                cap.browser_version + " on " +
-                cap.os + " " +
-                cap.os_version + " resolution:" +
-                cap.resolution);
-            customLaunchers[browser] = cap;
-            return browser;
-        }).filter((_, i) => i % (process.env.CIRCLE_NODE_TOTAL || 1) == process.env.CIRCLE_NODE_INDEX): [],
-        browserStack: {
-            project: (process.env.CIRCLE_PROJECT_REPONAME || "frame") + "_" + (process.env.CIRCLE_BRANCH || require("child_process").execSync("git branch | grep \\* | cut -d \" \" -f2-"))
-        },
-        customLaunchers: customLaunchers
+        frameworks,
+        reporters,
+        files,
+        preprocessors,
+        webpack,
+        beforeMiddleware,
+        concurrency,
+        browsers,
+        browserStack,
+        customLaunchers
     });
 };
