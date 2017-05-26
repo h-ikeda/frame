5cb3b78bc97c2246a1b358cc6b13433c4b3727e5
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2017-05-27 07:48:50 +0900

    IEでString.prototype.startsWith()メソッドが使えなかったので、indexOfで代用しました。

--- test/test_src/test_vuex-store/test_index.js
+++ test/test_src/test_vuex-store/test_index.js
@@ -231,7 +231,7 @@ describe("Vuexストアオプションのテスト", function() {
                 // The method is a string containing the name of the method to be invoked.
                 // Method names that begin with the word rpc followed by a period character (U+002E or ASCII 46) are reserved for rpc-internal methods and extensions and MUST NOT be used for anything else.
                 assert.equal(typeof parsedBody.method, "string");
-                assert(!parsedBody.method.startsWith("rpc."));
+                assert.notEqual(parsedBody.method.indexOf("rpc."), 0);
                 // The params is a structured value that holds the parameter values to be used during the invocation of the method. This member MAY be omitted.
                 if ("params" in parsedBody) {
                     assert.equal(typeof parsedBody.params, "object");
