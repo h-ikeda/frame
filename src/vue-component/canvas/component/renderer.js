5be72c6345485b64f14b7c3e18d0ade0eb061787
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2017-06-14 02:14:19 +0000

    算出プロパティを利用してWebGLRendererをインスタンス化するタイミングをvueにお任せ。

--- src/vue-component/canvas/component/renderer.js
+++ src/vue-component/canvas/component/renderer.js
@@ -12,6 +12,18 @@ export default {
         "depth",
         "logarithmicDepthBuffer"
     ],
+    computed: {
+        renderer() {
+            const options = {canvas: this.$el};
+            Object.keys(this.$props).forEach((prop) => {
+                if (typeof prop !== "undefined") {
+                    options[prop] = this[prop];
+                }
+            });
+            console.log("renderer created.");
+            return new WebGLRenderer(options);
+        }
+    },
     beforeCreate() {
         //
         // THREE.jsオブジェクトの受け渡しとレンダリング。
@@ -20,7 +32,7 @@ export default {
         // このコンポーネント内からはthis.$emit("イベント名", 渡すもの)で、
         // 子コンポーネントからはthis.$parent.$emit("イベント名", 渡すもの)で呼び出します。
         //
-        let scene, camera, renderer;
+        let scene, camera;
         
         // 子コンポーネントからオブジェクトを受け取って代入します。
         // THREE.Cameraのサブクラスであればobj.isCamera === trueです。
@@ -32,37 +44,19 @@ export default {
             }
         });
 
-        // THREE.WebGLRendererをrendererに代入します。
-        // 初期化時およびオプション変数を変更したときに呼び出します。
-        this.$on("createRenderer", () => {
-            const options = {
-                canvas: this.$el
-            };
-            // コンポーネントに渡されたプロパティのうち、undefinedでないものをオプションに指定。
-            Object.keys(this.$props).forEach((prop) => {
-                if (typeof prop !== "undefined") {
-                    options[prop] = this[prop];
-                }
-            });
-            renderer = new WebGLRenderer(options);
-        });
-
         // canvas要素へのレンダリングを実行します。
         this.$on("render", () => {
-            if (renderer) {
-                renderer.render(scene, camera);
-            }
+            this.renderer.render(scene, camera);
         });
         
         // canvasのサイズが変更された時に呼び出します。
         this.$on("resize", () => {
-            renderer.setSize(this.$el.clientWidth, this.$el.clientHeight, false);
+            this.renderer.setSize(this.$el.clientWidth, this.$el.clientHeight, false);
             camera.aspect = this.$el.clientWidth / this.$el.clientHeight;
             camera.updateProjectionMatrix();
         });
     },
     mounted() {
-        this.$emit("createRenderer");
         this.$emit("resize");
         this.$emit("render");
     },
