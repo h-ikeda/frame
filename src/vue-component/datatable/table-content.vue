ca6b432df921120622e21d14e92888db8317ceff
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-07-25 10:21:29 +0900

    テーブルのヘッダーとコンテンツをコンポーネント化。

---
+++ src/vue-component/datatable/table-content.vue
@@ -0,0 +1,43 @@
+<template>
+    <tbody class="mdc-typography--body1">
+        <tr v-for="item of data" :class="{selected: item.selected}" @click="item.toggleSelect">
+            <td>
+                <mdc-checkbox :checked="item.selected" />
+            </td>
+            <td v-for="value of item.columns">{{value}}</td>
+        </tr>
+    </tbody>
+</template>
+
+<script>
+    import mdcCheckbox from "../mdc-component/checkbox";
+    export default {
+        props: ["data"],
+        components: {
+            ...mdcCheckbox
+        }
+    };
+</script>
+
+<style scoped>
+    tr {
+        height: calc(3rem - 1px);
+        border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+    }
+    tr:hover {
+        background: #eeeeee;
+    }
+    .selected {
+        background: #f5f5f5;
+    }
+    td {
+        padding: 0 0 0 3.5rem;
+    }
+    td:nth-child(-n+2) {
+        padding-left: .8125rem;
+        width: 1px;
+    }
+    td:last-child {
+        padding-right: 1.5rem;
+    }
+</style>
--- src/vue-component/datatable/table-inner.vue
+++ src/vue-component/datatable/table-inner.vue
@@ -1,29 +1,16 @@
 <template>
     <component :is="selected">
         <template scope="data">
-            <thead class="mdc-typography--caption">
-                <tr>
-                    <th>
-                        <mdc-checkbox v-model="data.header.selectedAll" />
-                    </th>
-                    <th v-for="column of data.header.columns">{{column}}</th>
-                </tr>
-            </thead>
-            <tbody class="mdc-typography--body1">
-                <tr v-for="item of data.body" :class="{selected: item.selected}" @click="item.toggleSelect">
-                    <td>
-                        <mdc-checkbox :checked="item.selected" />
-                    </td>
-                    <td v-for="value of item.columns">{{value}}</td>
-                </tr>
-            </tbody>
+            <column-header :columns="data.header.columns" :checked="data.header.selectedAll" />
+            <table-content :data="data.body" />
         </template>
     </component>
 </template>
 
 <script>
     import {mapState} from "vuex";
-    import mdcCheckbox from "../mdc-component/checkbox";
+    import columnHeader from "./column-header.vue";
+    import tableContent from "./table-content.vue";
     import input from "./input";
     import result from "./result";
     import prefixed from "prefix-keys";
@@ -36,7 +23,8 @@
                 ...input,
                 ...result
             }),
-            ...mdcCheckbox
+            "column-header": columnHeader,
+            "table-content": tableContent
         }
     };
 </script>
@@ -48,31 +36,5 @@
         text-align: right;
         min-width: 100%;
         white-space: nowrap;
-    }
-    tr {
-        height: calc(3rem - 1px);
-        border-bottom: 1px solid rgba(0, 0, 0, 0.12);
-    }
-    thead tr {
-        height: calc(3.5rem - 1px);
-    }
-    tbody tr:hover {
-        background: #eeeeee;
-    }
-    .selected {
-        background: #f5f5f5;
-    }
-    th {
-        color: rgba(0, 0, 0, 0.54);
-    }
-    th, td {
-        padding: 0 0 0 3.5rem;
-    }
-    th:nth-child(-n+2), td:nth-child(-n+2) {
-        padding-left: .8125rem;
-        width: 1px;
-    }
-    th:last-child, td:last-child {
-        padding-right: 1.5rem;
     }
 </style>
