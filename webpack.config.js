8602d075a5507664cd6c8098f8959e27e5b96675
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-08-02 08:06:44 +0900

    .babelrcファイルを追加。

--- webpack.config.js
+++ webpack.config.js
@@ -23,13 +23,7 @@ module.exports = {
             //
             test: /\.vue$/,
             use: [{
-                loader: "vue-loader",
-                options: {
-                    loaders: {
-                        scss: "vue-style-loader!css-loader!sass-loader",
-                        sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax"
-                    }
-                }
+                loader: "vue-loader"
             }]
         }, {
             //
