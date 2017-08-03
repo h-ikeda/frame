932dbb1ffa91a0ff0c4b4d2dd220061219fc008f
Hiroki IKEDA <ikeda_hiroki@icloud.com>
2017-08-03 21:41:55 +0900

    consoleのoverrideのためにeslint ruleを記述。

--- test/index.js
+++ test/index.js
@@ -1,7 +1,10 @@
-["log", "warn", "error"].map((funcName)=>{
+// eslint no-console:0
+
+// three.jsのnull文字を含むログがkarma-junit-reporterからXMLStringifyに渡されるとエラーとなるので、null文字をreplaceしておく。
+["log", "warn", "error"].map((funcName) => {
     console[funcName + "old"] = console[funcName];
     console[funcName] = function(funcName, ...args) {
-        console[funcName + "old"].apply(console, args.map((arg)=>("" + arg)
+        console[funcName + "old"].apply(console, args.map((arg) => ("" + arg)
             .replace(/[\u0000-\u0008\u000B-\u000C\u000E-\u001F\uD800-\uDFFF\uFFFE-\uFFFF]/g,"_")));
     }.bind(console, funcName);
 });
