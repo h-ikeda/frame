4070bb374d4f2351ea81ee23480ce715f5101c9b
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2017-08-04 04:25:30 +0000

    browserstackはCIでのみ利用。

--- karma.conf.js
+++ karma.conf.js
@@ -2,29 +2,25 @@
 
 module.exports = (config) => {
 
-    const frameworks = ["polyfill", "mocha"];
-    const reporters = ["coverage-istanbul", "progress"];
-    const files = ["test/index.js"];
-    const preprocessors = {
-        "test/index.js": ["webpack", "sourcemap"]
+    const options = {
+        frameworks: ["polyfill", "mocha"],
+        reporters: ["coverage-istanbul"],
+        files: ["test/index.js"],
+        preprocessors: {
+            "test/index.js": ["webpack", "sourcemap"]
+        },
+        webpack: require("./webpack.config"),
+        beforeMiddleware: ["webpackBlocker"]
     };
-    const webpack = require("./webpack.config");
-    const beforeMiddleware = ["webpackBlocker"];
-    let concurrency;
-    const browsers = [];
-    const customLaunchers = {};
-    const browserStack = {
-        project: "frame_" + require("child_process").execSync("git branch | grep \\* | cut -d \" \" -f2-")
-    };
-    let browserNoActivityTimeout;
-    let port;
-    let browserDisconnectTolerance;
 
-    if (config.singleRun && process.env.BROWSER_STACK_USERNAME) {
-        reporters.push("BrowserStack");
-        concurrency = 1;
-        browserNoActivityTimeout = 30000;
-        browserDisconnectTolerance = 3;
+    if (process.env.CIRCLECI) {
+        options.reporters.push("junit", "BrowserStack");
+        options.junitReporter = {
+            outputDir: process.env.CIRCLE_TEST_REPORTS + "/junit/"
+        };
+        options.concurrency = 1;
+        options.browserNoActivityTimeout = 30000;
+        options.browserDisconnectTolerance = 3;
         const bsCaps = require("browserstack-capabilities")(process.env.BROWSER_STACK_USERNAME, process.env.BROWSER_STACK_ACCESS_KEY);
         const capabilities = bsCaps.create([{
             "browser": ["chrome", "firefox", "ie", "opera", "edge"],
@@ -37,6 +33,8 @@ module.exports = (config) => {
             "os": "OS X",
             "os_version": ["Sierra", "El Capitan"]
         }]).filter((capability) => capability);
+        const browsers = [];
+        const customLaunchers = {};
         capabilities.forEach((capability) => {
             const browser = [
                 "os",
@@ -48,40 +46,18 @@ module.exports = (config) => {
             capability.base = "BrowserStack";
             customLaunchers[browser] = capability;
         });
-    }
-
-    let junitReporter;
-
-    if (process.env.CIRCLECI) {
-        reporters.push("junit");
-        junitReporter = {
-            outputDir: process.env.CIRCLE_TEST_REPORTS + "/junit/"
-        };
         browsers.sort();
         const browserNum = Math.ceil(browsers.length / process.env.CIRCLE_NODE_TOTAL);
         browsers.splice(0, browserNum * process.env.CIRCLE_NODE_INDEX);
         browsers.splice(browserNum * (process.env.CIRCLE_NODE_INDEX + 1));
-        browserStack.project = process.env.CIRCLE_PROJECT_REPONAME + "_" + process.env.CIRCLE_BRANCH;
-        port = 9999 - process.env.CIRCLE_NODE_INDEX;
+        options.browserStack = {
+            project: process.env.CIRCLE_PROJECT_REPONAME + "_" + process.env.CIRCLE_BRANCH
+        };
+        options.port = 9999 - process.env.CIRCLE_NODE_INDEX;
     }
     else {
-        browserStack.build = Date.now();
+        options.reporters.push("progress");
     }
 
-    config.set({
-        frameworks,
-        reporters,
-        files,
-        preprocessors,
-        webpack,
-        beforeMiddleware,
-        concurrency,
-        browsers,
-        browserStack,
-        customLaunchers,
-        browserNoActivityTimeout,
-        port,
-        browserDisconnectTolerance,
-        junitReporter
-    });
+    config.set(options);
 };
