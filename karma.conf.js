688b3b87899df81362299d7bc40143bb90a53a48
IKEDA Hiroki <ikeda.hiroki.123@gmail.com>
2017-08-07 05:10:21 +0000

    Improve codestyle.

--- karma.conf.js
+++ karma.conf.js
@@ -65,28 +65,28 @@ module.exports = (config) => {
             case "1":
                 // Test on SauceLabs
                 customLaunchers = {
-                    sl_chrome: {
-                        base: 'SauceLabs',
-                        browserName: 'chrome',
-                        platform: 'Windows 7',
-                        version: '35'
+                    slChrome: {
+                        base: "SauceLabs",
+                        browserName: "chrome",
+                        platform: "Windows 7",
+                        version: "35"
                     },
-                    sl_firefox: {
-                        base: 'SauceLabs',
-                        browserName: 'firefox',
-                        version: '30'
+                    slFirefox: {
+                        base: "SauceLabs",
+                        browserName: "firefox",
+                        version: "30"
                     },
-                    sl_ios_safari: {
-                        base: 'SauceLabs',
-                        browserName: 'iphone',
-                        platform: 'OS X 10.9',
-                        version: '7.1'
+                    slSafari: {
+                        base: "SauceLabs",
+                        browserName: "safari",
+                        platform: "macOS 10.12",
+                        version: "10.0"
                     },
-                    sl_ie_11: {
-                        base: 'SauceLabs',
-                        browserName: 'internet explorer',
-                        platform: 'Windows 8.1',
-                        version: '11'
+                    slIE11: {
+                        base: "SauceLabs",
+                        browserName: "internet explorer",
+                        platform: "Windows 8.1",
+                        version: "11"
                     }
                 };
                 options.customLaunchers = customLaunchers;
